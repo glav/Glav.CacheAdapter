5f94ea45dd455546e064dff242ad29f2d08ea670
Paul Glavich <glav@theglavs.com>
2011-05-23 07:20:52 +1000

    Simplification of interfaces

--- Glav.CacheAdapter/CacheProvider.cs
+++ Glav.CacheAdapter/CacheProvider.cs
@@ -23,22 +23,6 @@ namespace Glav.CacheAdapter.Core
         }
         #region ICacheProvider Members
 
-        public object GetObject(string cacheKey, DateTime expiryDate, GetDataToCacheDelegate<object> getData)
-        {
-            object data = _cache.Get(cacheKey);
-            if (data == null)
-            {
-                data = getData();
-                _cache.Add(cacheKey, expiryDate, data);
-                _logger.WriteInfoMessage(string.Format("Adding item [{0}] to cache with expiry date/time of [{1}].", cacheKey, expiryDate.ToString("dd/MM/yyyy hh:mm:ss")));
-            }
-            else
-            {
-                _logger.WriteInfoMessage(string.Format("Retrieving item [{0}] from cache.", cacheKey));
-            }
-            return data;
-        }
-
         public T Get<T>(string cacheKey, DateTime expiryDate, GetDataToCacheDelegate<T> getData) where T : class
         {
             //Get data from cache
@@ -51,7 +35,7 @@ namespace Glav.CacheAdapter.Core
                 //only add non null data to the cache.
                 if (data != null)
                 {
-                    _cache.Add<T>(cacheKey, expiryDate, data);
+                    _cache.Add(cacheKey, expiryDate, data);
                     _logger.WriteInfoMessage(string.Format("Adding item [{0}] to cache with expiry date/time of [{1}].", cacheKey, expiryDate.ToString("dd/MM/yyyy hh:mm:ss")));
                 }
             }
@@ -63,7 +47,7 @@ namespace Glav.CacheAdapter.Core
                 //only add non null data to the cache.
                 if (!IsNullOrEmptyArray<T>(data))
                 {
-                    _cache.Add<T>(cacheKey, expiryDate, data);
+                    _cache.Add(cacheKey, expiryDate, data);
                     _logger.WriteInfoMessage(string.Format("Adding item [{0}] to cache with expiry date/time of [{1}].", cacheKey, expiryDate.ToString("dd/MM/yyyy hh:mm:ss")));
                 }
             }
@@ -74,7 +58,42 @@ namespace Glav.CacheAdapter.Core
             return data;
         }
 
-        private bool IsNullOrEmptyArray<T>(T data)
+		public T Get<T>(string cacheKey, TimeSpan slidingExpiryWindow, GetDataToCacheDelegate<T> getData) where T : class
+		{
+			//Get data from cache
+			T data = _cache.Get<T>(cacheKey);
+			if (data == null)
+			{
+				//get data from source
+				data = getData();
+
+				//only add non null data to the cache.
+				if (data != null)
+				{
+					_cache.Add(cacheKey, slidingExpiryWindow, data);
+					_logger.WriteInfoMessage(string.Format("Adding item [{0}] to cache with sliding sliding expiry window in seconds [{1}].", cacheKey, slidingExpiryWindow.ToString("dd/MM/yyyy hh:mm:ss")));
+				}
+			}
+			else if (IsNullOrEmptyArray<T>(data))
+			{
+				//get data from source
+				data = getData();
+
+				//only add non null data to the cache.
+				if (!IsNullOrEmptyArray<T>(data))
+				{
+					_cache.Add(cacheKey, slidingExpiryWindow, data);
+					_logger.WriteInfoMessage(string.Format("Adding item [{0}] to cache with sliding expiry window in seconds [{1}].", cacheKey, slidingExpiryWindow.TotalSeconds));
+				}
+			}
+			else
+			{
+				_logger.WriteInfoMessage(string.Format("Retrieving item [{0}] from cache.", cacheKey));
+			}
+			return data;
+		}
+		
+		private bool IsNullOrEmptyArray<T>(T data)
         {
             bool result = false;
             if (data is Array)
--- Glav.CacheAdapter/ICache.cs
+++ Glav.CacheAdapter/ICache.cs
@@ -8,12 +8,9 @@ namespace Glav.CacheAdapter.Core
 {
     public interface ICache
     {
-        void Add<T>(string cacheKey, DateTime absoluteExpiry, T dataToAdd) where T : class;
-		void Add<T>(string cacheKey, TimeSpan slidingExpiryWindow, T dataToAdd) where T : class;
         T Get<T>(string cacheKey) where T : class;
         void Add(string cacheKey, DateTime absoluteExpiry, object dataToAdd);
 		void Add(string cacheKey, TimeSpan slidingExpiryWindow, object dataToAdd);
-        object Get(string cacheKey);
         void InvalidateCacheItem(string cacheKey);
     	void AddToPerRequestCache(string cacheKey, object dataToAdd);
     }
