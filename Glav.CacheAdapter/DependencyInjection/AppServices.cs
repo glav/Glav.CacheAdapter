b99512b55681ed7e8282635451b7d8f6051c1c10
glav <glav@localhost>
2011-05-23 07:52:53 +1000

    Removed dependency on Unity and further simplified code

--- Glav.CacheAdapter/DependencyInjection/AppServices.cs
+++ Glav.CacheAdapter/DependencyInjection/AppServices.cs
@@ -2,50 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Practices.Unity;
+using Glav.CacheAdapter.Bootstrap;
+using Glav.CacheAdapter.Distributed;
+using Glav.CacheAdapter.Web;
+using Glav.CacheAdapter.Core.Diagnostics;
 
 namespace Glav.CacheAdapter.Core.DependencyInjection
 {
     public static class AppServices
     {
-        private static bool _isInitialised;
-        private static IUnityContainer _container;
+    	private static ICacheProvider _cacheProvider;
+    	private static ICache _cache;
+    	private static ILogging _logger;
 
-        public static IUnityContainer Container
-        {
-            get { return _container; }
-        }
-
+    	static AppServices()
+    	{
+    		PreStartInitialise();
+    	}
         /// <summary>
         /// Initialise the container with core dependencies.
         /// </summary>
         /// <remarks>Note: In a .Net 4 web app, this method could be invoked using the new PreApplicationStartMethod attribute
         /// as in: <code>[assembly: PreApplicationStartMethod(typeof(MyStaticClass), "PreStartInitialise")]</code>
+        /// Also note this section should be replaced with the DependencyInjection container of choice. This
+        /// code simply acts as a cheap mechanism for this without requiring a dependency on a 
+        /// container that you dont like/use.
         /// </remarks>
         public static void PreStartInitialise()
         {
-            if (_isInitialised)
-                return;
+        	_logger = new Logger();
+			switch (MainConfig.Default.CacheToUse.ToLowerInvariant())
+			{
+				case CacheTypes.MemoryCache:
+					_cache = new MemoryCacheAdapter(_logger);
+					break;
+				case CacheTypes.WebCache:
+					_cache = new WebCacheAdapter(_logger);
+					break;
+				case CacheTypes.AppFabricCache:
+					_cache = new AppFabricCacheAdapter(_logger);
+					break;
+				default:
+					_cache = new MemoryCacheAdapter(_logger);
+					break;
+			}
+			_cacheProvider = new CacheProvider(_cache,_logger);
+		}
 
-            // Initialise the IoC container
-            var bootstrapper = new ContainerBootStrapper();
-            _container = bootstrapper.InitialiseContainer();
-
-            _isInitialised = true;
-        }
-
-        /// <summary>
-        /// A convenience method so that consumers do not have to include Microsoft.Practices as a using
-        /// statement to be able to use the strongly typed extension methods.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="overrides"></param>
-        /// <returns></returns>
-        public static T Resolve<T>(params ResolverOverride[] overrides)
-        {
-            if (_container == null)
-                PreStartInitialise();
-            return _container.Resolve<T>(overrides);
-        }
+		public static ICacheProvider Cache { get { return _cacheProvider; } }
     }
 }
