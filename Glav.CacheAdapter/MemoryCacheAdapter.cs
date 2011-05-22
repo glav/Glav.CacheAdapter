86f2e23f245e4695709489ca36306991f1faec6e
glav <glav@localhost>
2011-05-23 08:07:38 +1000

    Added logging support to all cache impl and cleaned up

--- Glav.CacheAdapter/MemoryCacheAdapter.cs
+++ Glav.CacheAdapter/MemoryCacheAdapter.cs
@@ -25,29 +25,23 @@ namespace Glav.CacheAdapter.Core
             _logger = logger;
         }
 
-        public void Add<T>(string cacheKey, DateTime expiry, T dataToAdd) where T : class
-        {
-            var policy = new CacheItemPolicy();
-            policy.AbsoluteExpiration = new DateTimeOffset(expiry);
+		public void Add(string cacheKey, DateTime expiry, object dataToAdd)
+		{
+			var policy = new CacheItemPolicy();
+			policy.AbsoluteExpiration = new DateTimeOffset(expiry);
 
-            if (dataToAdd != null)
-                _cache.Add(cacheKey, dataToAdd, policy);
-        }
+			if (dataToAdd != null)
+			{
+				_cache.Add(cacheKey, dataToAdd, policy);
+				_logger.WriteInfoMessage(string.Format("Adding data to cache with cache key: {0}, expiry date {1}", cacheKey, expiry.ToString("yyyy/MM/dd hh:mm:ss")));
 
-        public T Get<T>(string cacheKey) where T : class
+			}
+		}
+
+    	public T Get<T>(string cacheKey) where T : class
         {
             T data = _cache.Get(cacheKey) as T;
             return data;
-        }
-
-        public void Add(string cacheKey, DateTime expiry, object dataToAdd)
-        {
-            Add<object>(cacheKey, expiry, dataToAdd);
-        }
-
-        public object Get(string cacheKey)
-        {
-            return _cache.Get(cacheKey);
         }
 
         public void InvalidateCacheItem(string cacheKey)
@@ -56,20 +50,16 @@ namespace Glav.CacheAdapter.Core
                 _cache.Remove(cacheKey);
         }
 
-
-		public void Add<T>(string cacheKey, TimeSpan slidingExpiryWindow, T dataToAdd) where T : class
+		public void Add(string cacheKey, TimeSpan slidingExpiryWindow, object dataToAdd)
 		{
 			if (dataToAdd != null)
 			{
 				var item = new CacheItem(cacheKey, dataToAdd);
 				var policy = new CacheItemPolicy() {SlidingExpiration = slidingExpiryWindow};
 				_cache.Add(item, policy);
-			}
-		}
+				_logger.WriteInfoMessage(string.Format("Adding data to cache with cache key: {0}, sliding expiry window in seconds {1}", cacheKey, slidingExpiryWindow.TotalSeconds));
 
-		public void Add(string cacheKey, TimeSpan slidingExpiryWindow, object dataToAdd)
-		{
-			Add<object>(cacheKey,slidingExpiryWindow,dataToAdd);
+			}
 		}
 
 		public void AddToPerRequestCache(string cacheKey, object dataToAdd)
