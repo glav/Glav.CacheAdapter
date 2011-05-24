ca733499f8e8912cc525752273612f7b83e11d5e
glav <glav@localhost>
2011-05-24 17:43:03 +1000

    Updated nuget package definition

--- Glav.CacheAdapter/Properties/AssemblyInfo.cs
+++ Glav.CacheAdapter/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Glav.CacheAdapter.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Glav.CacheAdapter")]
+[assembly: AssemblyDescription(".Net Cache level abstraction supporting memory, web and AppFabric cache")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Glav")]
-[assembly: AssemblyProduct("Glav.CacheAdapter.Core")]
-[assembly: AssemblyCopyright("Copyright © Glav 2010")]
+[assembly: AssemblyProduct("Glav.CacheAdapter")]
+[assembly: AssemblyCopyright("Copyright © Glav 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
