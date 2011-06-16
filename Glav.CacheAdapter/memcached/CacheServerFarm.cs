d53defcfec4d96b3e02eb7ec825043dc592bb1e5
glav <glav@localhost>
2011-06-16 23:14:05 +1000

    Addition of readme and start of memcached support

---
+++ Glav.CacheAdapter/memcached/CacheServerFarm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Glav.CacheAdapter.memcached
+{
+	class CacheServerFarm
+	{
+		private const int NUMBER_OF_KEYS = 100;
+
+		Dictionary<uint, ServerNode> _serverFarmKeys = new Dictionary<uint, ServerNode>();
+		private uint[] _allKeys;
+
+		public void BuildCacheFarmHashKeys(List<ServerNode> nodes)
+		{
+			var keys = new uint[nodes.Count * NUMBER_OF_KEYS];
+
+			int nodeIdx = 0;
+
+			foreach (ServerNode node in nodes)
+			{
+				var tmpKeys = GenerateCacheServerKeys(node);
+
+				for (var i = 0; i < tmpKeys.Length; i++)
+				{
+					_serverFarmKeys[tmpKeys[i]] = node;
+				}
+
+				tmpKeys.CopyTo(keys, nodeIdx);
+				nodeIdx += NUMBER_OF_KEYS;
+			}
+
+			Array.Sort<uint>(keys);
+			Interlocked.Exchange(ref _allKeys, keys);
+		}
+
+		public ServerNode FindCacheServerNodeForKey(string key)
+		{
+			if (_serverFarmKeys.Count == 0) return null;
+
+			uint itemKeyHash = BitConverter.ToUInt32(new DistributedFNV().ComputeHash(Encoding.UTF8.GetBytes(key)), 0);
+			int foundIndex = Array.BinarySearch<uint>(_serverFarmKeys.Keys.ToArray(), itemKeyHash);
+
+			// no exact match was found in the BinarySearch
+			if (foundIndex < 0)
+			{
+				// The Binary search provides a negative value which is the bitwise complement of the nearest match so we need to 
+				// to "flip" the value to get the positive index
+				foundIndex = ~foundIndex;
+
+				if (foundIndex == 0)
+				{
+					foundIndex = _serverFarmKeys.Count - 1;
+				}
+				else if (foundIndex >= _serverFarmKeys.Count)
+				{
+					foundIndex = 0;
+				}
+			}
+
+			if (foundIndex < 0 || foundIndex > _serverFarmKeys.Count)
+				return null;
+
+			return _serverFarmKeys[_allKeys[foundIndex]];
+		}
+
+		/// <summary>
+		/// We generate a set of potential keys for a server node based off its address. We then later use these keys to get
+		/// a match when searching for the cache key hash, or a close match
+		/// </summary>
+		/// <param name="node"></param>
+		/// <returns></returns>
+		private uint[] GenerateCacheServerKeys(ServerNode node)
+		{
+			const int KeyLength = 4;
+
+			var keyList = new uint[NUMBER_OF_KEYS];
+
+			string address = node.GetFullHostAddress();
+			var fnv = new DistributedFNV();
+
+			for (int keyCount = 0; keyCount < NUMBER_OF_KEYS; keyCount++) 
+			{
+				byte[] data = fnv.ComputeHash(Encoding.ASCII.GetBytes(String.Concat(address, "-", keyCount)));
+
+				keyList[keyCount] = BitConverter.ToUInt32(data, 0);
+			}
+
+			return keyList;
+		}
+
+	}
+
+	public class ServerNode
+	{
+		public ServerNode()
+		{
+			IsAlive = true;
+		}
+		public string IPAddress { get; set; }
+		public string Port { get; set; }
+		public bool IsAlive { get; set; }
+
+		public string GetFullHostAddress()
+		{
+			if (string.IsNullOrWhiteSpace(Port))
+				Port = "11211";
+			if (string.IsNullOrWhiteSpace(IPAddress))
+				IPAddress = "127.0.0.1";
+
+			return string.Format("{0}:{1}", IPAddress, Port);
+		}
+	}
+}
---
+++ Glav.CacheAdapter/memcached/FnvHash.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Security.Cryptography;
+
+namespace Glav.CacheAdapter.memcached
+{
+
+	/// <summary>
+	/// Implements a modified FNV hash with good distributiuon (better distribution than FNV1)
+	/// </summary>
+	/// <remarks>Algorithm found at http://bretm.home.comcast.net/hash/6.html</remarks>
+	public class DistributedFNV : HashAlgorithm
+	{
+		const uint PRIME = 16777619;
+		const uint INIT = 2166136261;
+
+		public DistributedFNV()
+		{
+			this.HashSizeValue = 32;
+		}
+
+
+		public override void Initialize()
+		{
+			CurrentHashValue = INIT;
+		}
+		
+		protected uint CurrentHashValue;
+		
+		protected override void HashCore(byte[] array, int ibStart, int cbSize)
+		{
+			int end = ibStart + cbSize;
+
+			for (int i = ibStart; i < end; i++)
+			{
+				CurrentHashValue ^= array[i];
+				CurrentHashValue *= PRIME;
+			}
+		}
+		
+		protected override byte[] HashFinal()
+		{
+			this.CurrentHashValue += this.CurrentHashValue << 13;
+			this.CurrentHashValue ^= this.CurrentHashValue >> 7;
+			this.CurrentHashValue += this.CurrentHashValue << 3;
+			this.CurrentHashValue ^= this.CurrentHashValue >> 17;
+			this.CurrentHashValue += this.CurrentHashValue << 5;
+
+			return BitConverter.GetBytes(this.CurrentHashValue);
+		}
+	}
+}
