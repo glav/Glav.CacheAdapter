96020e003d95c97fd5a370d1bc767be9ecdb4658
Paul Glavich <glav@theglavs.com>
2011-06-27 07:35:53 +1000

    Updated nuGet package again

--- Glav.CacheAdapter.ExampleUsage/Program.cs
+++ Glav.CacheAdapter.ExampleUsage/Program.cs
@@ -6,6 +6,11 @@ using Glav.CacheAdapter.Core.DependencyInjection;
 
 namespace Glav.CacheAdapter.ExampleUsage
 {
+	/// <summary>
+	/// This class is simply a very basic demonstration of the use of the CacheAdapter
+	/// in an application. This class is not required in any way so feel free to delete or modify
+	/// this class as you see fit.
+	/// </summary>
     class Program
     {
         static void Main(string[] args)
