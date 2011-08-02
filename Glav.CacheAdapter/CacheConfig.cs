3f57005a20229e7138d583dcda69146fc57f168f
glav <glav@localhost>
2011-08-02 22:32:35 +1000

    Created configuration common class

---
+++ Glav.CacheAdapter/CacheConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glav.CacheAdapter.Distributed;
+
+namespace Glav.CacheAdapter
+{
+	public class CacheConfig
+	{
+		private List<ServerNode> _serverNodes = new List<ServerNode>();
+		public List<ServerNode> ServerNodes { get { return _serverNodes; } }
+
+		private List<KeyValuePair<string,string>> _providerSpecificValues = new List<KeyValuePair<string, string>>();
+		public List<KeyValuePair<string, string>> ProviderSpecificValues { get { return _providerSpecificValues; } }
+
+	}
+}
