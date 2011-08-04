3fab21e6521957da8511c93bc3801eee3aceefa1
Paul Glavich <glav@theglavs.com>
2011-08-04 22:25:25 +1000

    Updated assemblies with latest APpFabric dependencies and added support for Auth tokens/msg security

--- Glav.CacheAdapter/Distributed/AppFabric/AppFabricCacheFactory.cs
+++ Glav.CacheAdapter/Distributed/AppFabric/AppFabricCacheFactory.cs
@@ -5,27 +5,24 @@ using System.Text;
 using Microsoft.ApplicationServer.Caching;
 using System.Diagnostics;
 using Glav.CacheAdapter.Core.Diagnostics;
+using System.Security;
 
 namespace Glav.CacheAdapter.Distributed.AppFabric
 {
     public class AppFabricCacheFactory: DistributedCacheFactoryBase
     {
-        private const string DEFAULT_ServerAddress = "localhost";
-    	public const string CONFIG_CacheNameKey = "DistributedCacheName";
-
-		private const int DEFAULT_Port = 22233;
-
         public AppFabricCacheFactory(ILogging logger) : base(logger)
         {
         }
 
         public DataCache ConstructCache()
         {
-            var config = ParseConfig(DEFAULT_ServerAddress,DEFAULT_Port);
+			var config = ParseConfig(AppFabricConstants.DEFAULT_ServerAddress, AppFabricConstants.DEFAULT_Port);
 			var dataCacheEndpoints = new List<DataCacheServerEndpoint>();
 			config.ServerNodes.ForEach(e => dataCacheEndpoints.Add(new DataCacheServerEndpoint(e.IPAddressOrHostName,e.Port)));
 
             var factoryConfig = new DataCacheFactoryConfiguration();
+			SetSecuritySettings(config, factoryConfig);
 
 			factoryConfig.Servers = dataCacheEndpoints;
 
@@ -34,17 +31,20 @@ namespace Glav.CacheAdapter.Distributed.AppFabric
 				var factory = new DataCacheFactory(factoryConfig);
                 DataCacheClientLogManager.ChangeLogLevel(System.Diagnostics.TraceLevel.Error);
 
+
 				// Note: When setting up AppFabric. The configured cache needs to be created by the admin using the New-Cache powershell command
             	string cacheName;
-				// Prefer the new config mechanismover the explicit entry but still support it
-				if (config.ProviderSpecificValues.ContainsKey(CONFIG_CacheNameKey))
+				// Prefer the new config mechanismover the explicit entry but still support it. So we
+				// try and extract config from the ProviderSpecificValues first.
+				if (config.ProviderSpecificValues.ContainsKey(AppFabricConstants.CONFIG_CacheNameKey))
 				{
-					cacheName = config.ProviderSpecificValues[CONFIG_CacheNameKey];
+					cacheName = config.ProviderSpecificValues[AppFabricConstants.CONFIG_CacheNameKey];
 				} else
 				{
 					cacheName = MainConfig.Default.DistributedCacheName;
 				}
 				var cache = factory.GetCache(cacheName);
+
                 return cache;
             }
             catch (Exception ex)
@@ -52,8 +52,57 @@ namespace Glav.CacheAdapter.Distributed.AppFabric
                 Logger.WriteException(ex);
                 throw;
             }
-            
         }
+
+		private void SetSecuritySettings(CacheConfig config, DataCacheFactoryConfiguration factoryConfig)
+		{
+			string securityModeValue = null;
+			// Set the security mode if required
+			if (config.ProviderSpecificValues.ContainsKey(AppFabricConstants.CONFIG_SecurityModeKey))
+			{
+				securityModeValue = config.ProviderSpecificValues[AppFabricConstants.CONFIG_SecurityModeKey];
+			}
+
+			// Set the authorization info/value if required
+			string securityAuthValue = null;
+			if (config.ProviderSpecificValues.ContainsKey(AppFabricConstants.CONFIG_SecurityMessageAuthorisationKey))
+			{
+				securityAuthValue = config.ProviderSpecificValues[AppFabricConstants.CONFIG_SecurityMessageAuthorisationKey];
+			}
+
+			string useSslValue = null;
+			if (config.ProviderSpecificValues.ContainsKey(AppFabricConstants.CONFIG_UseSslKey))
+			{
+				useSslValue = config.ProviderSpecificValues[AppFabricConstants.CONFIG_UseSslKey];
+			}
+
+			var normalisedSecurityMode = string.IsNullOrWhiteSpace(securityModeValue) ? string.Empty : securityModeValue.ToLowerInvariant();
+			var normalisedSslValue = string.IsNullOrWhiteSpace(useSslValue) ? string.Empty : useSslValue.ToLowerInvariant();
+			if (!string.IsNullOrWhiteSpace(securityAuthValue))
+			{
+				if (normalisedSecurityMode == AppFabricConstants.CONFIG_SecurityMode_Message)
+				{
+					var secureToken = new SecureString();
+					foreach (var ch in securityAuthValue)
+					{
+						secureToken.AppendChar(ch);
+					}
+					bool useSsl = false;
+					if (normalisedSslValue == "true" || normalisedSslValue == "1")
+					{
+						useSsl = true;
+					}
+					DataCacheSecurity securityProps = new DataCacheSecurity(secureToken, useSsl);
+					factoryConfig.SecurityProperties = securityProps;
+				}
+
+				if (normalisedSecurityMode == AppFabricConstants.CONFIG_SecurityMode_Transport)
+				{
+					DataCacheSecurity securityProps = new DataCacheSecurity(DataCacheSecurityMode.Transport,DataCacheProtectionLevel.None);
+					factoryConfig.SecurityProperties = securityProps;
+				}
+			}
+		}
 
     }
 }
