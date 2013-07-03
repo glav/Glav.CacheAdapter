502e5a0df9d140c06f09c4b7e92b1e8f8a3841f6
Paul Glavich <glav@theglavs.com>
2013-07-04 06:49:51 +1000

    Added extra nested/heirarchical dependency invalidation test

--- Glav.CacheAdapter.Tests/DependencyManagementTests.cs
+++ Glav.CacheAdapter.Tests/DependencyManagementTests.cs
@@ -116,5 +116,68 @@ namespace Glav.CacheAdapter.Tests
             var cacheKeyForParent = string.Format("{0}{1}{2}", GenericDependencyManager.CacheKeyPrefix, GenericDependencyManager.CacheDependencyEntryPrefix, PARENTKEYNAME);
             Assert.IsNotNull(cache.Get<DependencyItem[]>(cacheKeyForParent));
         }
+
+        [TestMethod]
+        public void ShouldInvalidateNestedDependenciesAndParent()
+        {
+            var mgr = TestHelper.GetDependencyManager();
+            var cacheProvider = TestHelper.GetCacheProvider();
+            var cache = TestHelper.GetCacheFromConfig();
+
+            // Make sure we start out with nothing
+            mgr.RemoveParentDependencyDefinition(PARENTKEYNAME);
+
+            // Setup parent cache Item
+            var parentData = cacheProvider.Get<string>(PARENTKEYNAME, DateTime.Now.AddDays(1), () => "ParentBlob");
+            // Setup 1st level dependent children cache items
+            var data1 = cacheProvider.Get<string>("Key1", DateTime.Now.AddDays(1), () => "Key1Data", PARENTKEYNAME);
+            var data2 = cacheProvider.Get<string>("Key2", DateTime.Now.AddDays(1), () => "Key2Data", PARENTKEYNAME);
+            var data3 = cacheProvider.Get<string>("Key3", DateTime.Now.AddDays(1), () => "Key3Data", PARENTKEYNAME);
+            Assert.IsNotNull(cache.Get<string>(PARENTKEYNAME));
+            Assert.IsNotNull(cache.Get<string>("Key1"));
+            Assert.IsNotNull(cache.Get<string>("Key2"));
+            Assert.IsNotNull(cache.Get<string>("Key3"));
+
+            // Setup 2nd level dependent children cache items
+            var level2_data1 = cacheProvider.Get<string>("Level2Key1", DateTime.Now.AddDays(1), () => "Key1Data", "Key1");
+            var level2_data2 = cacheProvider.Get<string>("Level2Key2", DateTime.Now.AddDays(1), () => "Key2Data", "Key1");
+            var level2_data3 = cacheProvider.Get<string>("Level2Key3", DateTime.Now.AddDays(1), () => "Key3Data", "Key2");
+            Assert.IsNotNull(cache.Get<string>("Level2Key1"));
+            Assert.IsNotNull(cache.Get<string>("Level2Key2"));
+            Assert.IsNotNull(cache.Get<string>("Level2Key3"));
+
+            // Setup 3rd level dependent children cache items
+            var level3_data1 = cacheProvider.Get<string>("Level3Key1", DateTime.Now.AddDays(1), () => "Key1Data", "Level2Key1");
+            var level3_data2 = cacheProvider.Get<string>("Level3Key2", DateTime.Now.AddDays(1), () => "Key2Data", "Level2Key2");
+            var level3_data3 = cacheProvider.Get<string>("Level3Key3", DateTime.Now.AddDays(1), () => "Key3Data", "Level2Key3");
+            Assert.IsNotNull(cache.Get<string>("Level3Key1"));
+            Assert.IsNotNull(cache.Get<string>("Level3Key2"));
+            Assert.IsNotNull(cache.Get<string>("Level3Key3"));
+
+            // Setup 4th level dependent children cache items
+            var level4_data1 = cacheProvider.Get<string>("Level4Key1", DateTime.Now.AddDays(1), () => "Key1Data", "Level3Key3");
+            Assert.IsNotNull(cache.Get<string>("Level4Key1"));
+
+            // Now invalidate the top level parent item
+            cacheProvider.InvalidateCacheItem(PARENTKEYNAME);
+
+            // Ensure all dependencies and children of children(nested) dependencies
+            // have also cleared
+            Assert.IsNull(cache.Get<string>(PARENTKEYNAME));
+            // 1st level
+            Assert.IsNull(cache.Get<string>("Key1"));
+            Assert.IsNull(cache.Get<string>("Key2"));
+            Assert.IsNull(cache.Get<string>("Key3"));
+            // 2nd level
+            Assert.IsNull(cache.Get<string>("Level2Key1"));
+            Assert.IsNull(cache.Get<string>("Level2Key2"));
+            Assert.IsNull(cache.Get<string>("Level2Key3"));
+            // 3rd level
+            Assert.IsNull(cache.Get<string>("Level3Key1"));
+            Assert.IsNull(cache.Get<string>("Level3Key2"));
+            Assert.IsNull(cache.Get<string>("Level3Key3"));
+            // 4th level
+            Assert.IsNull(cache.Get<string>("Level4Key1"));
+        }
     }
 }
