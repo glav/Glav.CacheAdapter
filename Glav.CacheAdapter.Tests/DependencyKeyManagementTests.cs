bd544ced319e3a544e0472a1210cbc25181adb03
glav <glav@localhost>
2013-07-04 06:07:26 +1000

    Refactored/simplified interface and fixed tests

--- Glav.CacheAdapter.Tests/DependencyKeyManagementTests.cs
+++ Glav.CacheAdapter.Tests/DependencyKeyManagementTests.cs
@@ -19,11 +19,11 @@ namespace Glav.CacheAdapter.Tests
             var mgr = TestHelper.GetDependencyManager();
 
             // Make sure we start out with nothing
-            mgr.RemoveParentItem(PARENTKEYNAME);
+            mgr.RemoveParentDependencyDefinition(PARENTKEYNAME);
 
-            mgr.RegisterParentItem(PARENTKEYNAME);
+            mgr.RegisterParentDependencyDefinition(PARENTKEYNAME);
 
-            var groupEntry = mgr.GetDependentCacheKeysForParent(PARENTKEYNAME);
+            var groupEntry = mgr.GetDependentCacheKeysForParent(PARENTKEYNAME,true);
             Assert.IsNotNull(groupEntry, "Did not get a parent entry");
             Assert.AreEqual<int>(1,groupEntry.Count());
             Assert.AreEqual<string>(PARENTKEYNAME, groupEntry.First().CacheKey);
@@ -35,59 +35,61 @@ namespace Glav.CacheAdapter.Tests
             var mgr = TestHelper.GetDependencyManager();
 
             // Make sure we start out with nothing
-            mgr.RemoveParentItem(PARENTKEYNAME);
+            mgr.RemoveParentDependencyDefinition(PARENTKEYNAME);
 
-            mgr.RegisterParentItem(PARENTKEYNAME);
+            mgr.RegisterParentDependencyDefinition(PARENTKEYNAME);
 
-            var groupEntry = mgr.GetDependentCacheKeysForParent(PARENTKEYNAME);
+            var groupEntry = mgr.GetDependentCacheKeysForParent(PARENTKEYNAME,true);
             Assert.IsNotNull(groupEntry, "Did not get a parent entry");
             Assert.AreEqual<int>(1, groupEntry.Count());
             Assert.AreEqual<string>(PARENTKEYNAME, groupEntry.First().CacheKey);
 
-            mgr.RemoveParentItem(PARENTKEYNAME);
+            // Remove it
+            mgr.RemoveParentDependencyDefinition(PARENTKEYNAME);
+            // Ensure we can get the entry (as it is implicitly created)
+            // but has not items
             groupEntry = mgr.GetDependentCacheKeysForParent(PARENTKEYNAME);
             Assert.IsNotNull(groupEntry);
+            Assert.AreEqual<int>(0, groupEntry.Count());
+            // Ensure we can also return the parent node definition when requested
+            groupEntry = mgr.GetDependentCacheKeysForParent(PARENTKEYNAME,true);
+            Assert.IsNotNull(groupEntry);
+            Assert.AreEqual<int>(1, groupEntry.Count());
         }
 
         [TestMethod]
-        public void ShouldRemoveCacheItemsInGroupFromCache()
+        public void ShouldClearDependentItemsInParentListFromListButNotRemoveParent()
         {
-            var cache = TestHelper.GetCacheFromConfig();
             var mgr = TestHelper.GetDependencyManager();
 
             // Make sure we start out with nothing
-            mgr.RemoveParentItem(PARENTKEYNAME);
+            mgr.RemoveParentDependencyDefinition(PARENTKEYNAME);
 
-            mgr.RegisterParentItem(PARENTKEYNAME);
+            mgr.RegisterParentDependencyDefinition(PARENTKEYNAME);
 
-            // Define our cache dependency groupmembers
-            mgr.AssociateDependentKeysToParent(PARENTKEYNAME, new string[3] {"Key1","Key2","Key3"});
+            mgr.AssociateDependentKeysToParent(PARENTKEYNAME, new string[3] {"one", "two", "three"});
 
-            // Add items to cache with same keys as per dependency group
-            cache.Add("Key1", DateTime.Now.AddDays(1),"Data1");
-            cache.Add("Key2", DateTime.Now.AddDays(1), "Data2");
-            cache.Add("Key3", DateTime.Now.AddDays(1), "Data3");
-
-            // Assert that the group is defined
+            // Not including parentNode
             var groupEntry = mgr.GetDependentCacheKeysForParent(PARENTKEYNAME);
             Assert.IsNotNull(groupEntry, "Did not get a parent entry");
+            Assert.AreEqual<int>(3, groupEntry.Count());
+
+            // Including the parentNode
+            groupEntry = mgr.GetDependentCacheKeysForParent(PARENTKEYNAME,true);
             Assert.AreEqual<int>(4, groupEntry.Count());
+            Assert.IsNotNull(groupEntry.FirstOrDefault(n => n.IsParentNode));
 
-            // Assert we have the items actually in the cache
-            Assert.IsNotNull(cache.Get<string>("Key1"));
-            Assert.AreEqual<string>("Data1", cache.Get<string>("Key1"));
-            Assert.IsNotNull(cache.Get<string>("Key2"));
-            Assert.AreEqual<string>("Data2", cache.Get<string>("Key2"));
-            Assert.IsNotNull(cache.Get<string>("Key3"));
-            Assert.AreEqual<string>("Data3", cache.Get<string>("Key3"));
+            // Clear the list
+            mgr.RemoveParentDependencyDefinition(PARENTKEYNAME);
+            // Get just the dependencies again, not including parent node
+            groupEntry = mgr.GetDependentCacheKeysForParent(PARENTKEYNAME);
+            Assert.IsNotNull(groupEntry);
+            Assert.AreEqual<int>(0, groupEntry.Count());
+            // And get all dependencies including parent node
+            groupEntry = mgr.GetDependentCacheKeysForParent(PARENTKEYNAME,true);
+            Assert.AreEqual<int>(1, groupEntry.Count());
 
-            //Now clear the dependencies
-            mgr.PerformActionForDependenciesAssociatedWithParent(PARENTKEYNAME);
-
-            // All items in group should be cleared from cache
-            Assert.IsNull(cache.Get<string>("Key1"));
-            Assert.IsNull(cache.Get<string>("Key2"));
-            Assert.IsNull(cache.Get<string>("Key3"));
         }
+
     }
 }
