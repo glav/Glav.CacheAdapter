cc5685e78331467db80d138a237b7a320412b992
Paul Glavich <glav@theglavs.com>
2013-11-06 16:17:41 +1100

    Added support for local cache properties

--- Glav.CacheAdapter/Distributed/AppFabric/AppFabricConstants.cs
+++ Glav.CacheAdapter/Distributed/AppFabric/AppFabricConstants.cs
@@ -15,6 +15,13 @@ namespace Glav.CacheAdapter.Distributed.AppFabric
 		public const string CONFIG_UseSslKey = "UseSsl";
 	    public const string CONFIG_ChannelOpenTimeout = "ChannelOpenTimeout";
 	    public const string CONFIG_MaxConnectionsToServer = "MaxConnectionsToServer";
+        
+        public const string CONFIG_LocalCache_IsEnabled = "LocalCache.IsEnabled";
+        public const string CONFIG_LocalCache_DefaultTimeout = "LocalCache.DefaultTimeout";
+        public const string CONFIG_LocalCache_ObjectCount = "LocalCache.ObjectCount";
+        public const string CONFIG_LocalCache_InvalidationPolicy = "LocalCache.InvalidationPolicy";
+        public const string CONFIG_LocalCache_InvalidationPolicyValue_TimeoutBased = "timeoutbased";
+        public const string CONFIG_LocalCache_InvalidationPolicyValue_NotificationBased = "notificationbased";
 
 		public const string CONFIG_SecurityMode_Message = "message";
 		public const string CONFIG_SecurityMode_None = "none";
