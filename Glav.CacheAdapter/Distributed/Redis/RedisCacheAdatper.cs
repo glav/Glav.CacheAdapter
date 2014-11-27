e81061887c15b3d569001178bb6124ddc9024f67
Paul Glavich <glav@theglavs.com>
2014-11-27 16:43:45 +1100

    Initial Redis CacheAdapter implementation

--- Glav.CacheAdapter/Distributed/Redis/RedisCacheAdatper.cs
+++ Glav.CacheAdapter/Distributed/Redis/RedisCacheAdatper.cs
@@ -1,9 +1,12 @@
 ﻿using Glav.CacheAdapter.Core;
 using Glav.CacheAdapter.Core.Diagnostics;
+using Glav.CacheAdapter.Web;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Glav.CacheAdapter.Helpers;
 
 namespace Glav.CacheAdapter.Distributed.Redis
 {
@@ -11,47 +14,93 @@ namespace Glav.CacheAdapter.Distributed.Redis
     {
 		private ILogging _logger;
         private RedisCacheFactory _factory;
-        
+        private PerRequestCacheHelper _requestCacheHelper = new PerRequestCacheHelper();
+        private static IDatabase _db = null;
 
         public RedisCacheAdatper(ILogging logger, CacheConfig config = null)
         {
             _logger = logger;
             _factory = new RedisCacheFactory(logger, config);
 
+            _db = _factory.ConstructCacheInstance();
         }
         public T Get<T>(string cacheKey) where T : class
         {
-            throw new NotImplementedException();
+            try
+            {
+                var requestCacheData = _requestCacheHelper.TryGetItemFromPerRequestCache<T>(cacheKey);
+                if (requestCacheData != null)
+                {
+                    return requestCacheData;
+                }
+
+                var data = _db.StringGet(cacheKey);
+                if (!data.IsNull && data.HasValue)
+                {
+                    var blobBytes = (byte[])data;
+                    var deserialisedObject = blobBytes.Deserialize<T>();
+                    return deserialisedObject;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.WriteException(ex);
+            }
+            return null;
         }
 
         public void Add(string cacheKey, DateTime absoluteExpiry, object dataToAdd)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var expiry = absoluteExpiry - DateTime.Now;
+                var success = _db.StringSet(cacheKey, dataToAdd.Serialize(), expiry);
+                if (!success)
+                {
+                    _logger.WriteErrorMessage(string.Format("Unable to store item in cache. CacheKey:{0}", cacheKey));
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.WriteException(ex);
+            }
         }
 
         public void Add(string cacheKey, TimeSpan slidingExpiryWindow, object dataToAdd)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var success = _db.StringSet(cacheKey, dataToAdd.Serialize(),slidingExpiryWindow);
+                if (!success)
+                {
+                    _logger.WriteErrorMessage(string.Format("Unable to store item in cache. CacheKey:{0}", cacheKey));
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.WriteException(ex);
+            }
         }
 
         public void InvalidateCacheItem(string cacheKey)
         {
-            throw new NotImplementedException();
+            _db.KeyDelete(cacheKey);
         }
 
         public void AddToPerRequestCache(string cacheKey, object dataToAdd)
         {
-            throw new NotImplementedException();
+            _requestCacheHelper.AddToPerRequestCache(cacheKey, dataToAdd);
         }
 
         public CacheSetting CacheType
         {
-            get { throw new NotImplementedException(); }
+            get { return CacheSetting.Redis; }
         }
 
         public void ClearAll()
         {
-            throw new NotImplementedException();
+            //TODO: Figure out good way of clearing a redis instance of data
+            _logger.WriteErrorMessage("Redis does not support clearing the entire contents of the cache.");
         }
     }
 }
