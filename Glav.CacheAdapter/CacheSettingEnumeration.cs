9d8bbbad31fb81df714d4cbb4efe53f68d23a4eb
Paul Glavich <glav@theglavs.com>
2014-11-27 16:43:45 +1100

    Initial Redis CacheAdapter implementation

--- Glav.CacheAdapter/CacheSettingEnumeration.cs
+++ Glav.CacheAdapter/CacheSettingEnumeration.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Glav.CacheAdapter.Bootstrap;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,35 @@ namespace Glav.CacheAdapter.Core
         Memory,
         Web,
         AppFabric,
-		memcached
+		memcached,
+        Redis
+    }
+
+    public static class CacheSettingExtensions
+    {
+        public static string ToStringType(this CacheSetting cacheSetting)
+        {
+            switch (cacheSetting)
+            {
+                case CacheSetting.AppFabric:
+                    return CacheTypes.AppFabricCache;
+                    break;
+                case CacheSetting.memcached:
+                    return CacheTypes.memcached;
+                    break;
+                case CacheSetting.Memory:
+                    return CacheTypes.MemoryCache;
+                    break;
+                case CacheSetting.Redis:
+                    return CacheTypes.redis;
+                    break;
+                case CacheSetting.Web:
+                    return CacheTypes.WebCache;
+                    break;
+                default:
+                    return CacheTypes.MemoryCache;
+                    break;
+            }
+        }
     }
 }
--- Glav.CacheAdapter/Distributed/Redis/RedisCacheFactory.cs
+++ Glav.CacheAdapter/Distributed/Redis/RedisCacheFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Glav.CacheAdapter.Core.Diagnostics;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +13,45 @@ namespace Glav.CacheAdapter.Distributed.Redis
         private const string DEFAULT_IpAddress = "127.0.0.1";
         private const int DEFAULT_Port = 6379;
 
+        private ConnectionMultiplexer _redisConnection = null;
+        private IDatabase _db = null;
+
         public RedisCacheFactory(ILogging logger, CacheConfig config = null) : base(logger,config)
         {
+            ParseConfig(DEFAULT_IpAddress, DEFAULT_Port);
+        }
+
+        public IDatabase ConstructCacheInstance()
+        {
+            var connectionOptions = ConstructConnectionOptions();
+
+            try
+            {
+                _redisConnection = ConnectionMultiplexer.Connect(connectionOptions);
+                _db = _redisConnection.GetDatabase();
+            } catch (Exception ex)
+            {
+                Logger.WriteException(ex);
+                throw;
+            }
+
+            return _db;
+        }
+
+        private ConfigurationOptions ConstructConnectionOptions()
+        {
+            // Note: The redis config parser requires values be separated by a comma, not a semi-colon which is what CacheAdapter likes
+            var redisSpecificOptions = string.IsNullOrWhiteSpace(CacheConfiguration.CacheSpecificData) ? string.Empty : CacheConfiguration.CacheSpecificData.Replace(';',',');
+            var redisOptions = ConfigurationOptions.Parse(redisSpecificOptions, true);
+
+            // Clear the endpoints if any specified here as we use the ones defined in DistributedCacheServers setting to keep
+            // config consistent and it means that users can switch to different cache providers without issues
+            redisOptions.EndPoints.Clear();
+            CacheConfiguration.ServerNodes.ForEach(n =>
+            {
+                redisOptions.EndPoints.Add(n.IPAddressOrHostName, n.Port);
+            });
+            return redisOptions;
         }
     }
 }
