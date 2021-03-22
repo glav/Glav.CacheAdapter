af6fd5157d7ae4abe4352686e42c7ac1fefcf41f
Paul Glavich <glav@theglavs.com>
2021-03-23 09:27:01 +1100

    Fixing stupid error on my part

--- Glav.CacheAdapter.Package/Packages/Glav.CacheAdapter/CreatePackage.cmd
+++ Glav.CacheAdapter.Package/Packages/Glav.CacheAdapter/CreatePackage.cmd
@@ -2,19 +2,18 @@ echo off
 echo.
 echo Copying latest files from Release directory
 
-mkdir lib > NUL:
-copy "..\..\..\Glav.CacheAdapter\bin\Release\Glav.CacheAdapter.dll" "lib\net45\" /y > NUL:
-copy "..\..\..\Glav.CacheAdapter\bin\Release\Glav.CacheAdapter.dll" "lib\net472\" /y > NUL:
-copy "..\..\..\Glav.CacheAdapter\Readme.txt" "content\CacheAdapter-ReadMe.txt" /y > NUL:
-copy "..\..\..\Glav.CacheAdapter.ExampleUsage\Program.cs" "content\CacheAdapterExamples\Example-CacheAdapterUsage.cs" /y > NUL:
-copy "..\..\..\Glav.CacheAdapter.ExampleUsage\ConsoleHelper.cs" "content\CacheAdapterExamples\ConsoleHelper.cs" /y > NUL:
+mkdir lib
+copy "..\..\..\Glav.CacheAdapter\bin\Release\Glav.CacheAdapter.dll" "lib\net472\" /y
+copy "..\..\..\Glav.CacheAdapter\Readme.txt" "content\CacheAdapter-ReadMe.txt" /y
+copy "..\..\..\Glav.CacheAdapter.ExampleUsage\Program.cs" "content\CacheAdapterExamples\Example-CacheAdapterUsage.cs" /y
+copy "..\..\..\Glav.CacheAdapter.ExampleUsage\ConsoleHelper.cs" "content\CacheAdapterExamples\ConsoleHelper.cs" /y
 
-copy "..\..\..\Glav.CacheAdapter.ExampleUsage\HammerTheCache.cs" "content\CacheAdapterExamples\HammerTheCache.cs" /y > NUL:
-copy "..\..\..\Glav.CacheAdapter.ExampleUsage\InMemoryLogger.cs" "content\CacheAdapterExamples\InMemoryLogger.cs" /y > NUL:
-copy "..\..\..\Glav.CacheAdapter.ExampleUsage\SimpleUsageAsync.cs" "content\CacheAdapterExamples\SimpleUsageAsync.cs" /y > NUL:
-copy "..\..\..\Glav.CacheAdapter.ExampleUsage\SimpleUsageWithDependencies.cs" "content\CacheAdapterExamples\SimpleUsageWithDependencies.cs" /y > NUL:
-copy "..\..\..\Glav.CacheAdapter.ExampleUsage\SimpleUsageWithTests.cs" "content\CacheAdapterExamples\SimpleUsageWithTests.cs" /y > NUL:
-copy "..\..\..\Glav.CacheAdapter.ExampleUsage\SomeData.cs" "content\CacheAdapterExamples\SomeData.cs" /y > NUL:
+copy "..\..\..\Glav.CacheAdapter.ExampleUsage\HammerTheCache.cs" "content\CacheAdapterExamples\HammerTheCache.cs" /y
+copy "..\..\..\Glav.CacheAdapter.ExampleUsage\InMemoryLogger.cs" "content\CacheAdapterExamples\InMemoryLogger.cs" /y
+copy "..\..\..\Glav.CacheAdapter.ExampleUsage\SimpleUsageAsync.cs" "content\CacheAdapterExamples\SimpleUsageAsync.cs" /y
+copy "..\..\..\Glav.CacheAdapter.ExampleUsage\SimpleUsageWithDependencies.cs" "content\CacheAdapterExamples\SimpleUsageWithDependencies.cs" /y
+copy "..\..\..\Glav.CacheAdapter.ExampleUsage\SimpleUsageWithTests.cs" "content\CacheAdapterExamples\SimpleUsageWithTests.cs" /y
+copy "..\..\..\Glav.CacheAdapter.ExampleUsage\SomeData.cs" "content\CacheAdapterExamples\SomeData.cs" /y
 
 echo ....Done.
 echo.
